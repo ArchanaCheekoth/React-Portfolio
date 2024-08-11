38d9fe78cfe59c9b3d949f186a1b6890c5a2ce7a
ArchanaCheekoth <archanacheekoth97@gmail.com>
2024-08-11 17:51:13 +0530

    css changes

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import "./project.css";
 import image from "../../assets/image/project.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExternalLink, faDownload } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
-const ProjectCard = ({data}) => {
+const ProjectCard = ({ data }) => {
   return (
     <div className="shadow-lg flip-card">
       <div className="flip-card-inner">
@@ -15,40 +15,39 @@ const ProjectCard = ({data}) => {
           <p className="tech text-center m-5">{data.tech}</p>
         </div>
         <div className="flip-card-back">
-          <p className="card-text">
-          {data.description}
-          </p>
-          
+          <p className="card-text">{data.description}</p>
+
           {/* {data.tech.map((i,d)=><p key={i}>{d}</p>)} */}
           <div className="social-buttons mt-5">
-                    <a href={data.githubLink}>
-                      {" "}
-                      <button className="neo-button">
-                        <FontAwesomeIcon
-                          icon={faGithub}
-                          style={{ fontSize: "24px" }}
-                        />{" "}
-                      </button>
-                    </a>
-                   {data.websiteLink!=null? <a href={data.websiteLink}>
-                      {" "}
-                      <button className="neo-button">
-                        <FontAwesomeIcon
-                          icon={faExternalLink}
-                          style={{ fontSize: "24px" }}
-                        />{" "}
-                      </button>
-                    </a>:null}
-                    {data.downloadLink!=null? <a href={data.downloadLink}>
-                      {" "}
-                      <button className="neo-button">
-                        <FontAwesomeIcon
-                          icon={faDownload}
-                          style={{ fontSize: "24px" }}
-                        />{" "}
-                      </button>
-                    </a>:null}
-                  </div>
+            <a href={data.githubLink}>
+              {" "}
+              <button className="neo-button">
+                <FontAwesomeIcon icon={faGithub} style={{ fontSize: "24px" }} />{" "}
+              </button>
+            </a>
+            {data.websiteLink != null ? (
+              <a href={data.websiteLink}>
+                {" "}
+                <button className="neo-button">
+                  <FontAwesomeIcon
+                    icon={faExternalLink}
+                    style={{ fontSize: "24px" }}
+                  />{" "}
+                </button>
+              </a>
+            ) : null}
+            {data.downloadLink != null ? (
+              <a href={data.downloadLink}>
+                {" "}
+                <button className="neo-button">
+                  <FontAwesomeIcon
+                    icon={faDownload}
+                    style={{ fontSize: "24px" }}
+                  />{" "}
+                </button>
+              </a>
+            ) : null}
+          </div>
         </div>
       </div>
     </div>
